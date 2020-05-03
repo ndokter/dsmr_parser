fc4a96ebab4a9a6a536e542407541cabcc0e7d5d
Hans Erik van Elburg <hanserik@vanelburg.net>
2020-05-03 11:18:08 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Nigel Dokter',
     author_email='nigel@nldr.net',
     url='https://github.com/ndokter/dsmr_parser',
-    version='0.18',
+    version='0.19',
     packages=find_packages(),
     install_requires=[
         'pyserial>=3,<4',
