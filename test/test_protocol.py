4688cf905862a02f26cc63484d4e5b0edf8f9440
Dennis Siemensma <github@dennissiemensma.nl>
2021-09-22 21:26:54 +0200

    test/test_protocol.py:6:1: W0611 'dsmr_parser.telegram_specifications' imported but unused [pyflakes] test/test_protocol.py:7:1: W0611 'dsmr_parser.parsers.TelegramParser' imported but unused [pyflakes]

--- test/test_protocol.py
+++ test/test_protocol.py
@@ -3,8 +3,6 @@ from unittest.mock import Mock
 import unittest
 
 from dsmr_parser import obis_references as obis
-from dsmr_parser import telegram_specifications
-from dsmr_parser.parsers import TelegramParser
 from dsmr_parser.clients.protocol import create_dsmr_protocol
 
 
