d2edb52efdc51da60e04fc62727a18322cb76b92
J. Nick Koston <nick@koston.org>
2024-06-07 09:01:54 -0500

    actually commit the import change

--- dsmr_parser/clients/rfxtrx_protocol.py
+++ dsmr_parser/clients/rfxtrx_protocol.py
@@ -2,7 +2,7 @@
 
 import asyncio
 
-from serial_asyncio import create_serial_connection
+from serial_asyncio_fast import create_serial_connection
 from .protocol import DSMRProtocol, _create_dsmr_protocol
 
 
