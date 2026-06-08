5f0a0a88d23983517077d481270c28604fd6717a
Nigel Dokter <mail@nldr.net>
2026-06-08 08:28:19 +0200

    Bump version from 1.7.0 to 1.8.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='mail@nldr.net',
     license='MIT',
     url='https://github.com/ndokter/dsmr_parser',
-    version='1.7.0',
+    version='1.8.0',
     packages=find_packages(exclude=('test', 'test.*')),
     install_requires=[
         'serialx>=1.8,<2',
